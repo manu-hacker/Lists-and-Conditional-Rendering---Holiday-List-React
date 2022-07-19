ba9285dcff57aa99df1112b81c17ceed03f9ed0a
Milind-Badekar <badekarmilind@gmail.com>
2022-07-19 13:27:09 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
+// import React, { Component } from "react";
+
+// export default App;
 import React, { Component } from "react";
+// import "../styles/App.css";
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      text: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
 
-      showPara: false,
-    };
+    this.cityList = [
+      { name: "Goa", country: "India" },
+      { name: "Amsterdam", country: "Netherlands" },
+      { name: "New York", country: "USA" },
+      { name: "Darjeeling", country: "India" },
+      { name: "Tokyo", country: "Japan" },
+      { name: "Lonavala", country: "India" },
+      // { name: 'Brandenburg Gate', country: 'Germany' },
+      // { name: 'Reichstag Building', country: 'Germany' },
+      // { name: 'Museum Island', country: 'Germany' },
+      // { name: 'Munnar', country: 'India' },
+      // { name: 'Leh Ladakh', country: 'India' },
+      // { name: 'Goa', country: 'India' },
+      // { name: 'Agra', country: 'India' },
+      // { name: 'Dalhousie', country: 'India' },
+      // { name: 'Coorg', country: 'India' },
+      // { name: 'Rome', country: 'Italy' },
+      // { name: 'Milan', country: 'Italy' },
+      // { name: 'Venice', country: 'Italy' },
+      // { name: 'Varanasai', country: 'India' },
+      // { name: 'Jaipur', country: 'India' },
+      // { name: 'The Hofburg', country: 'Austria' },
+      // { name: 'Belvedere Palace', country: 'Austria' },
+      // { name: 'St. Stephen Cathedral', country: 'Austria' },
+      // { name: 'Kahna National Park', country: 'India' },
+      // { name: 'Amritsar', country: 'India' },
+      // { name: 'Mussoorie', country: 'India' },
+      // { name: 'Mount Abu', country: 'India' },
+      // { name: 'Tirupati', country: 'India' },
+    ];
   }
-
-  onClick = () => {
-    // this.setState((this.state.showPara = true));
-    this.setState({ showPara: true });
-  };
 
   render() {
     return (
       <div id="main">
-        {/* Do not remove this main div!! */}
-        <button id="click" onClick={this.onClick}>
-          Button
-        </button>
-        {this.state.showPara && <p id="para">{this.state.text}</p>}
+        <ol>
+          {this.cityList
+            .filter((city) => city.country === "India")
+            .map((city, index) => {
+              const key = `location${index + 1}`;
+              return <li key={key}>{city.name}</li>;
+            })}
+        </ol>
       </div>
     );
   }
