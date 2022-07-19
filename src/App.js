d6ed28588d3c4f4095aac06fb8baeb7d88b30c3b
Milind-Badekar <badekarmilind@gmail.com>
2022-07-19 11:47:56 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,84 +1,29 @@
-import React, { Component, useState, useEffect, useCallback } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
 
-const App = () => {
-  const [renderBall, setRenderBall] = useState(false);
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [ballPosition, setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
-  });
-
-  const start = () => {
-    setRenderBall(!renderBall);
-  };
-
-  const keyHandler = (event) => {
-    let ballPositionClone = { ...ballPosition };
-
-    if (event.keyCode === 37) {
-      ballPositionClone.left =
-        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) -
-        5 +
-        "px";
-    } else if (event.keyCode === 39) {
-      ballPositionClone.left =
-        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) +
-        5 +
-        "px";
-    } else if (event.keyCode === 38) {
-      ballPositionClone.top =
-        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) -
-        5 +
-        "px";
-    } else if (event.keyCode === 40) {
-      ballPositionClone.top =
-        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) +
-        5 +
-        "px";
-    }
-    // ballPositionClone.left = (Number(ballPosition.left.slice(0,ballPosition.left.indexOf("p")))+5)+"px";
-    setBallPosition(ballPositionClone);
-    // console.log(ballPosition);
-  };
-
-  useEffect(() => {
-    window.addEventListener("keydown", keyHandler);
-    return () => window.removeEventListener("keydown", keyHandler);
-  });
-
-  const reset = () => {
-    setRenderBall(!renderBall);
-    setBallPosition({ left: "0px", top: "0px" });
-  };
-  const renderChoice = () => {};
-
-  let compo;
-  if (renderBall) {
-    compo = (
-      <div
-        className="ball"
-        style={{ left: ballPosition.left, top: ballPosition.top }}
-      ></div>
-    );
-  } else {
-    compo = (
-      <button onClick={start} className="start">
-        Start
-      </button>
-    );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
+      click: 0,
+    };
   }
 
-  return (
-    <div className="playground">
-      {compo}
-      <button onClick={reset} className="reset">
-        Reset
-      </button>
-      {renderChoice()}
-    </div>
-  );
-};
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <button
+          id="click"
+          onClick={() => this.setState({ count: this.state.count + 1 })}
+        >
+          Button
+        </button>
+        <h6 id="para">{this.state.text}</h6>
+      </div>
+    );
+  }
+}
 
 export default App;
